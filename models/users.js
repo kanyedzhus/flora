c87da082cf2c2bbb684e2370395beba0dfba08ef
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-17 12:05:58 +0100

    seeds

--- models/users.js
+++ models/users.js
@@ -10,9 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Users.hasMany(models.address)
+      Users.hasOne(models.address)
       Users.hasOne(models.buyers)
-      Users.hasMany(models.sellers)
+      Users.hasOne(models.sellers)
 
     }
   }
