3945b76c0188d9aef20bfbf2db3fdd591519d8f9
JoyAnneW <joyawilliams@gmail.com>
2022-06-17 12:40:27 +0200

    wrapped App in CartContext and ProductsContext providers.

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import CartContextProvider from "./contexts/cart-context";
+import ProductsContextProvider from "./contexts/products-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<ProductsContextProvider>
+				<CartContextProvider>
+					<App />
+				</CartContextProvider>
+			</ProductsContextProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
