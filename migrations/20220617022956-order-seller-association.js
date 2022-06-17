46fe1cfe5c267e776dca88182f413c61f9104662
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-17 03:32:50 +0100

    more keys

---
+++ migrations/20220617022956-order-seller-association.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.addConstraint("orders", {
+      fields: ["sellerId"],
+      type: "foreign key",
+      name: "order_seller_association",
+      references:{
+        table: "sellers",
+        field: "sellerId"
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.removeConstraint("categories", "catrder_seller_association")
+  }
+};
