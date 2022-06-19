14dba693ae129f36662c6893dbda1b7cb1230aff
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-19 18:07:51 +0100

    seeds

---
+++ seeders/20220619165943-demo-buyer.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+     await queryInterface.bulkInsert('People', [{
+      "buyerId":1,"userId":1,"credit":0},
+{"buyerId":2,"userId":4,"credit":0,
+"createdAt":"2022-01-17",
+"updatedAt":"2022-06-17"},
+{"buyerId":3,"userId":6,"credit":2,
+"createdAt":"2022-01-17",
+"updatedAt":"2022-06-17"},
+{"buyerId":4,"userId":7,"credit":5,
+"createdAt":"2022-01-17",
+"updatedAt":"2022-06-17"},
+{"buyerId":5,"userId":10,"credit":1,
+"createdAt":"2022-01-17",
+"updatedAt":"2022-06-17"
+       
+      }], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("sellers", null, {});
+  }
+};
