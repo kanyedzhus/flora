4e4d4d897cf207d41b60b41b78eec73d46477bba
JoyAnneW <joyawilliams@gmail.com>
2022-06-20 15:36:00 +0200

    set up multer in back end. added dynamic year to footer

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var sellersRouter = require('./routes/sellers');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var sellersRouter = require("./routes/sellers");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+// needed to make this folder static inorder to access the file in the browser using its name
+app.use("/public/files", express.static("public/files"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/sellers', sellersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/sellers", sellersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.send("error");
 });
 
 module.exports = app;
---
+++ routes/products.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../model/helper");
+// this package allows us to handle FormData (different content type) - both text and files
+const multer = require("multer");
+
+// for adjusting how files get stored. multer will execute these functions whenever a new file is received
+const storage = multer.diskStorage({
+	// these functions have access to the info about the file in the file object (2nd arg)
+	destination: function (req, file, cb) {
+		// cb takes an error as first argument and the path for storage as second
+		cb(null, "./public/files/");
+	},
+	filename: function (req, file, cb) {
+		// this configures the filename in the db. Adding date infront of originalname protects against potentially overwriting files
+		cb(null, Date.now() + "-" + file.originalname);
+	},
+});
+
+// configuring multer. Can add properties for file limits, type, size etc
+const upload = multer({ storage: storage });
+
+// for creating a product in our db. Will need to edit this to suit specific details. "the name passed to upload.single will need to be matched in the frontend and sent in the payload
+router.post("/", upload.single("product_image"), async (req, res, next) => {
+	// info about the file uploaded is in req.file, which multer makes available to us.
+	console.log(req.file, req.body);
+	const { filename, path } = req.file;
+
+	// windows file paths  use only backslash, but the forward slash is needed to make the file url accessible. this regular expression will find all back slashes and replace with forward slash in path.
+	const correctedPath = path.replace(/\\/g, "/");
+
+	// destructure necessary info from req.body to make insert into db.
+});
