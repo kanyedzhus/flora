f4879077113a0d8524c2b7e37269161083cf3485
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-20 10:49:16 +0100

    server and relationships fix

---
+++ routes/sellers.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var models = require ("../models")
+
+router.get("/", (req, res) =>{
+   
+    models.sellers.findAll({attributes: ['sellerId']})
+       .then((seller)=> res.send(seller))
+       .catch((err) =>res.status(500).send({error: err.message}));
+    });
+
+// router.post("/", (req, res, next){
+// const {} = req.body;
+
+// models.products.create {[]}
+// .then((product)=> res.send(product))
+// .cath((err) =>res.status(500).send(err));
+// })
+
+
+module.exports = router;
