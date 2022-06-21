7bbdb0bfbd74eb56c6dec6181150b3adafc9805d
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-21 11:04:00 +0100

    seller-router

--- routes/sellers.js
+++ routes/sellers.js
@@ -4,16 +4,16 @@ var models = require ("../models")
 
 //Get all sellers
 router.get("/", (req, res) =>{
-    
     models.sellers.findAll({attributes: ['sellerId', 'userId', 'storeName','storeDescription']})
     .then((seller)=> res.send(seller))
     .catch((err) =>res.status(500).send({error: err.message}));
 });
 
 //Get seller by Id
-router.get("/:id",(req, res) =>{
-    const {sellerId} = req.body;
-        models.sellers.findOne({ where: {sellerId,}},{
+router.get("/:sellerId",(req, res) =>{
+    const {sellerId} = req.params;
+    console.log(req.params)
+        models.sellers.findOne({ where: {sellerId}},{
             attributes: ['sellerId', 'userId', 'storeName','storeDescription']})
         .then((seller)=> res.send(seller))
         .catch((err) =>res.status(500).send({error: err.message}));
@@ -21,38 +21,37 @@ router.get("/:id",(req, res) =>{
 
 //Get all products
 router.get("/product", (req, res) =>{
-   
     models.products.findAll({attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
-       .then((seller)=> res.send(seller))
+       .then((product)=> res.send(product))
        .catch((err) =>res.status(500).send({error: err.message}));
     });
 
 //Get products by id
-router.get("/product/:id",(req, res) =>{
-const {productId} = req.params.id;
-    models.products.findAll({ where: {productId,}},{
-        attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
-    .then((seller)=> res.send(seller))
-    .catch((err) =>res.status(500).send({error: err.message}));
-});
+// router.get("/product/:id",(req, res) =>{
+// const {productId} = req.params.id;
+//     models.products.findAll({ where: {productId,}},{
+//         attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
+//     .then((seller)=> res.send(seller))
+//     .catch((err) =>res.status(500).send({error: err.message}));
+// });
 
 //Get products by category Id
-router.get("/product/category/:id",(req, res) =>{
-    const {categoryId} = req.params.id;
-        models.sellers.findAll({ where: {categoryId,}},{
-            attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
-        .then((seller)=> res.send(seller))
-        .catch((err) =>res.status(500).send({error: err.message}));
-    });
+// router.get("/product/category/:id",(req, res) =>{
+//     const {categoryId} = req.params.id;
+//         models.sellers.findAll({ where: {categoryId,}},{
+//             attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
+//         .then((seller)=> res.send(seller))
+//         .catch((err) =>res.status(500).send({error: err.message}));
+//     });
 
 //Get products by seller Id
-router.get("/product/sellers/:id",(req, res) =>{
-    const {sellerId} = req.params.id;
-        models.sellers.findAll({ where: {sellerId,}},{
-            attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
-        .then((seller)=> res.send(seller))
-        .catch((err) =>res.status(500).send({error: err.message}));
-    });
+// router.get("/product/sellers/:id",(req, res) =>{
+//     const {sellerId} = req.params.id;
+//         models.sellers.findAll({ where: {sellerId,}},{
+//             attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
+//         .then((seller)=> res.send(seller))
+//         .catch((err) =>res.status(500).send({error: err.message}));
+//     });
 
 // Get orders by seller Id
 
