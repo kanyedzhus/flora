fd5d87c3d2d3de226b7ec4a890d0c63c6d54b88d
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-22 11:51:57 +0100

    post

--- routes/sellers.js
+++ routes/sellers.js
@@ -13,21 +13,33 @@ router.get("/", async (req, res) =>{
     
 });
 
+//Get all products
+router.get("/product", (req, res) =>{
+    models.product.findAll({attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
+       .then((product)=> res.send(product))
+       .catch((err) =>res.status(500).send({error: err.message}));
+    });
+
 //Get seller by Id
 router.get("/:sellerId",(req, res) =>{
     const {sellerId} = req.params;
-    console.log(req.params)
+    
         models.seller.findOne({ 
             attributes: ['sellerId', 'userId', 'storeName','storeDescription'], where: {sellerId}})
         .then((seller)=> res.send(seller))
         .catch((err) =>res.status(500).send({error: err.message}));
     });
 
-//Get all products
-router.get("/product", (req, res) =>{
-    models.product.findAll({attributes: ['productId', 'categoryId', 'sellerId','description','color','dimensionsCM','imgURL','price','quantity','careDifficulty','light','petFriendly','airPurifying' ]})
-       .then((product)=> res.send(product))
-       .catch((err) =>res.status(500).send({error: err.message}));
+
+    //Post a Product
+    router.post("/product", (req, res) =>{
+      const {categoryId, sellerId, description, color, dimensionsCM, imgURL, price, quantity, careDifficulty, light, petFriendly,airPurifying} = req.body;
+      //console.log({categoryId, sellerId, description, color, dimensionsCM, imgURL, price, quantity, careDifficulty, light, petFriendly,airPurifying});
+       
+        models.product.create({categoryId, sellerId, description, color, dimensionsCM, imgURL, price, quantity, careDifficulty, light, petFriendly,airPurifying}).then((data) => res.send(data))
+        .catch((error) => {
+        res.status(500).send(error);
+    });
     });
 
 //Get products by id
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,40 @@ var router = express.Router();
 var models = require ("../models")
 
 /* GET users listing. */
- router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
- });
+//  router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+//  });
 
-// router.get("/user", async (req, res) =>{ 
-//   try {
-//       const response = await models.user.findAll({attributes:["userId","userName","email","firstName","lastName","role"]});
-//       res.send(response);
-//     } catch (err) {
-//       res.status(400).send({ message: err.message });
-//     }
+// Get all users
+router.get("/", async (req, res) =>{ 
+  try {
+      const response = await models.user.findAll({attributes:["userId","userName","email","firstName","lastName","role"]});
+      res.send(response);
+    } catch (err) {
+      res.status(400).send({ message: err.message });
+    }
   
-// });
+});
+
+//Post user
+router.post("/", (req, res) =>{
+  const {userName, email, hashedPassword, firstName, lastName, imgUrl, role, stripeId } = req.body;
+   
+    models.user.create({userName, email, hashedPassword, firstName, lastName, imgUrl, role, stripeId}).then((data) => res.send(data))
+    .catch((error) => {
+    res.status(500).send(error);
+});
+});
+
+// get all addresses
+router.get("/address", async (req, res) =>{ 
+  try {
+      const response = await models.address.findAll({attributes:["addressId","userId","line1","line2","city","postalCode","country"]});
+      res.send(response);
+    } catch (err) {
+      res.status(400).send({ message: err.message });
+    }
+  
+});
 
 module.exports = router;
