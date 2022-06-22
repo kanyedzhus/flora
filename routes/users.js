d7fb1c411ab3b037e2c7d6d0184e5daae5441915
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-22 09:33:31 +0100

    new routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var models = require ("../models")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+ });
+
+// router.get("/user", async (req, res) =>{ 
+//   try {
+//       const response = await models.user.findAll({attributes:["userId","userName","email","firstName","lastName","role"]});
+//       res.send(response);
+//     } catch (err) {
+//       res.status(400).send({ message: err.message });
+//     }
+  
+// });
 
 module.exports = router;
