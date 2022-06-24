5c6427842ce61199b630ac19fe4accea4f066466
MariaBia-Maclagan <82051716+MariaBia-Maclagan@users.noreply.github.com>
2022-06-24 18:48:43 +0100

    order router

--- routes/orders.js
+++ routes/orders.js
@@ -23,7 +23,7 @@ router.get("/:oderId", async (req, res) =>{
       } catch (err) {
         res.status(400).send({ message: err.message });
       }
-    })
+    });
 
 //Get all orders by sellerId
 // orders/order/:sellerId
@@ -35,6 +35,53 @@ router.get("/order/:sellerId",async (req, res) =>{
       } catch (err) {
         res.status(400).send({ message: err.message });
       }
-    })
+    });
+
+     //Post an order
+     router.post("/", (req, res) =>{
+      const {orderId, buyerId, sellerId, invoiceId, status, total} = req.body;
+   
+        models.order.create({orderId, buyerId, sellerId, invoiceId, status, total})
+        .then((data) => res.send(data))
+        .catch((error) => {
+        res.status(500).send(error);
+    });
+    });
+
+    //Put order status by order Id
+    router.put("/:orderId", async (req, res) => {
+      const { orderId } = req.params;
+      const { status } = req.body;
+    
+      try {
+        const status = await models.order.findOne({
+          where: {
+            orderId,
+          },
+        });
+    
+        const data = await status.update({where: {status}});
+    
+        res.send(data);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    });
+
+    //Get order item buy orderId
+    router.get("/item/:orderId", async (req, res) =>{ 
+      const {orderId} = req.params;
+      try {
+          const response = await models.orderItem.findAll({where: {orderId}, attributes:["oderItemId","orderId","productId","quantity","createdAt"]});
+          res.send(response);
+        } catch (err) {
+          res.status(400).send({ message: err.message });
+        }
+      });
+
+
+    //Post order item
+  router.post("/item", )
+  
 
     module.exports = router;
