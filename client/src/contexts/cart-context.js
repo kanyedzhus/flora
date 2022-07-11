ab624e1a469e0ce4bf982c4af9def9c58d3fcae6
JoyAnneW <joyawilliams@gmail.com>
2022-07-11 16:27:13 +0200

    added buyerId to functions that run on successful payment. accessing buyer via context, not props

--- client/src/contexts/cart-context.js
+++ client/src/contexts/cart-context.js
@@ -1,13 +1,15 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { fetchFromAPI } from "../../src/helpers";
 import { toast } from "react-toastify";
+import { BuyerContext } from "./buyer-context";
 
 export const CartContext = createContext();
 
 export default function CartContextProvider({ children }) {
+	const { buyer } = useContext(BuyerContext);
 	useEffect(() => {
 		getCartItems();
-		getCartSession();
+		getCartSession(buyer.buyerId);
 	}, []);
 
 	// *ADD TO CART
